79b6a3e3d1a92707329d78c034be2cc3d5eaa4e1
Javier Pereyra <javier.pereyra@gmail.com>
2020-09-06 23:10:57 -0300

    Clase ventana

--- src/ejemplo1/Principal.java
+++ src/ejemplo1/Principal.java
@@ -1,10 +1,13 @@
 package ejemplo1;
 
+import javax.swing.JFrame;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Test");
+
+		
 	}
 
 }
